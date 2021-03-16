6a80e6e6a48de3b83718a3d1ab6a3e55d3f57836
Vik-17 <zhakevichv@ya.ru>
2021-03-16 15:57:39 +0300

    load optimization

--- js/main.js
+++ js/main.js
@@ -22,8 +22,14 @@
   menuCloseItem.addEventListener("click", () => {
     menu.classList.remove("header__nav_active");
   });
+  if (window.innerWidth <= 767) {
+    for (let i = 0; i < menuLinks.length; i += 1) {
+      menuLinks[i].addEventListener("click", () => {
+        menu.classList.remove("header__nav_active");
+      });
+    }
+  }
 })();
-
 // Scroll to anchors
 (function () {
   const smoothScroll = function (targetEl, duration) {
